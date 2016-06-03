e660f572a2271de5544b6a4204d063975db5d95a
trion129 <parmsingh129@yahoo.com>
2016-06-03 11:49:57 +0530

    Completed

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-var express = require("express");
+var express = require('express')
+var multer  = require('multer')
+var path = require("path")
+var upload = multer({ dest: 'uploads/' })
+ 
+var app = express()
+ 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-var app = express();
-
-app.enable('trust proxy');
-
-app.get("/",function(req,res){
-	var object = {};
-	object.ip = req.ip;
-	object.language = req.headers["accept-language"][0];
-	object.os = req.headers['user-agent'].match(/\((.*?)\)/gi)[0];
-	object.os = object.os.substring(1,object.os.length-1);
-	res.end(JSON.stringify(object));
+app.post('/', multer({ dest: './uploads/'}).single('upl'), function(req,res){
+	res.write(JSON.stringify(req.file)); //form files
+	/* example output:
+            { fieldname: 'upl',
+              originalname: 'grumpy.png',
+              encoding: '7bit',
+              mimetype: 'image/png',
+              destination: './uploads/',
+              filename: '436ec561793aa4dc475a88e84776b1b9',
+              path: 'uploads/436ec561793aa4dc475a88e84776b1b9',
+              size: 277056 }
+	 */
+	res.status(204).end();
 });
 
 app.listen(process.env.PORT || 8080);
